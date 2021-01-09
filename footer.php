473526f640da5a08bea163dee0572b182d41c2f8
ganeshkode <49275234+ganeshkode@users.noreply.github.com>
2021-01-09 20:17:43 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+               <div class="box footer">
+                  <footer class="line">
+                     <div class="s-12 l-6">
+                        <p>Copyright 2019</p>
+                     </div>
+                     <div class="s-12 l-6">
+                 Design and coding by Ganesh Kode, Avinash Singh, Khan Hasan Raza, Sharayu Mane, Rutuja Jadhav.
+              </div>
+                  </footer>
+               </div>
+            </div>
+         </div>
+      </div>
+	
+                 
+              
